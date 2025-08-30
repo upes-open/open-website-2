1d7cd8dd2566558ff13a7cd9722f2fb470034f9c
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-08-30 22:20:05 +0530

    create: eslint and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,36 @@
+import js from "@eslint/js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default tseslint.config(
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: [".next", "dist", "node_modules", "out", "build", "next-env.d.ts"],
   },
-];
-
-export default eslintConfig;
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  ...compat.config({ extends: ["next/core-web-vitals", "next/typescript"] }),
+  eslintConfigPrettier,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "prefer-const": [
+        "error",
+        { destructuring: "all", ignoreReadBeforeAssign: true },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { ignoreRestSiblings: true },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  }
+);
