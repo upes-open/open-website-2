3b9554a9322842643c513f5dc1a929d778d977c8
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-09-01 20:18:09 +0530

    update redirect page layout

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -2,6 +2,9 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 const REDIRECT_URL = 'https://google.com';
 
@@ -22,10 +25,28 @@ export default function RedirectPage() {
   }, [router]);
 
   return (
-    <div className='flex items-center justify-center h-screen bg-black'>
-      <p className='text-white text-2xl'>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='flex items-center -ml-8'>
+        <Image
+          src='/logo.png'
+          alt='logo'
+          width={75}
+          height={75}
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          className='mr-2'
+        />
+        <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold'>
+          OPEN COMMUNITY
+        </h1>
+      </div>
+      <h3 className='text-base font-medium'>
         Redirecting to {REDIRECT_URL} in {counter}...
-      </p>
+      </h3>
+      <div className='mt-8'>
+        <Link href='/'>
+          <Button variant='default'>Go to Home</Button>
+        </Link>
+      </div>
     </div>
   );
 }
